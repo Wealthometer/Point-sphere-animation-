c2c573e6ba7fc5f07aac5156bf2170b2be17591b
Wealthometer <wealthhajoh87@gmail.com>
2025-08-04 10:46:11 +0100

    Update Script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let scene = new THREE.Scene();
-document.addEventListener('mousemove', onMouseMove, false);
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+document.addEventListener("mousemove", onMouseMove, false);
+let camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 let mouseX;
 let mouseY;
@@ -9,28 +14,37 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-window.addEventListener("resize", function() {
+window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-
-
 let distance = Math.min(200, window.innerWidth / 4);
 let geometry = new THREE.Geometry();
 
-for(let i = 0; i < 1600; i++){
-    let vertex = new THREE.Vector3();
+for (let i = 0; i < 1600; i++) {
+  let vertex = new THREE.Vector3();
 
-    let theta = THREE.Math.randFloatSpread(360);
-    let phi = THREE.Math.randFloatSpread(360);
+  let theta = THREE.Math.randFloatSpread(360);
+  let phi = THREE.Math.randFloatSpread(360);
 
-    vertex.x = distance * Math.sin(theta) * Math.cos(phi);
-    vertex.y = distance * Math.sin(theta) * Math.cos(phi);
-    vertex.z = distance * Math.cos(theta);
+  vertex.x = distance * Math.sin(theta) * Math.cos(phi);
+  vertex.y = distance * Math.sin(theta) * Math.cos(phi);
+  vertex.z = distance * Math.cos(theta);
 
-    geometry.verticesPush(vertex);
-
-    let particles = new THREE.Points(geometry, new THREE.PointsMaterial())
+  geometry.verticesPush(vertex);
 }
+
+let particles = new THREE.Points(
+  geometry,
+  new THREE.PointsMaterial({ color: 0 * ff44ff, size: 2 })
+);
+
+particles.boundingSphere = 50;
+
+let renderingParent = new THREE.Group();
+renderingParent.add(particles);
+
+let resizeContainer = new THREE.Group();
+
